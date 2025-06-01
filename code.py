cc229ca39713ee3d6bcf54adb1bbb933c9165aa8
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-06-02 05:20:34 +0700

    Update code.py

--- code.py
+++ code.py
@@ -12,7 +12,7 @@ CSV_URL = f"https://docs.google.com/spreadsheets/d/1viV03CJxPsK42zZyKI6ZfaXlLR62
 
 # --- YOUR WEBHOOK URLs ---
 WEBHOOK_URL_PHOTO = "https://script.google.com/macros/s/AKfycbyds4um-AHIqGc32-92SbGw1FOfufUPF5QnfqE9CZUpjrw_iAinLwpZgJK8KhFhnUzAsg/exec"
-WEBHOOK_URL_DATA = "https://script.google.com/macros/s/AKfycbxYsW_Ldiojn5Y-sMSS0zL5bc_8T2rTmC_cEQxw7Bna_K4SzpKTGPRe7IWhVrgfGXD-IA/exec"
+WEBHOOK_URL_DATA = "https://script.google.com/macros/s/AKfycbxKlQbLQDjUKaOwTN6NqpqsV2weV5VnYoMIhZCP44H9xiK5BoaBuDdMb6BBHOmCWoscwg/exec"
 
 # --- Load PO Data from Google Sheet ---
 @st.cache_data
