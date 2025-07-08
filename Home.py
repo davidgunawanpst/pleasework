ee0ae5a2c8609c4aa026f5db20bfca8efc18b272
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-08 09:30:56 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Warehouse Operations Suite", layout="wide")
+st.title("📦 Welcome to the Warehouse App")
+st.write("Use the sidebar to navigate through different warehouse modules.")
