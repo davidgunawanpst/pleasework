a3bf2bde3a6e44f6d5358cbac9175ba89733431e
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-08 09:35:25 +0700

    Delete Manual Receive.py

--- Manual Receive.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="Manual Receive", layout="wide")
-st.title("📝 Manual Receive")
-st.write("This is a placeholder for the Manual Receive module.")
